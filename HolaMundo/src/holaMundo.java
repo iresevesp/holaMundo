43b63f121cc3559cbd340d76b35f3e75ea59e3ca
iresevesp <150520920+iresevesp@users.noreply.github.com>
2023-11-15 19:20:35 +0100

    Add files via upload

---
+++ HolaMundo/src/holaMundo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner (System.in);
+	
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+	}
+
+}
